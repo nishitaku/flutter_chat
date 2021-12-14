e7f4527e2d583bf42687c7d3faef3fc9ad793d6d
nishitaku <29690066+nishitaku@users.noreply.github.com>
2021-12-14 23:07:33 +0900

    implements chat card

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/pages/add_post_page.dart';
@@ -28,8 +29,46 @@ class ChatPage extends StatelessWidget {
           )
         ],
       ),
-      body: Center(
-        child: Text('ログイン情報:${user.email}'),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: Text('ログイン情報:${user.email}'),
+          ),
+          Expanded(
+            // FutureBuilder
+            // 非同期処理の結果を元にWidgetを作れる
+            child: FutureBuilder<QuerySnapshot>(
+              // 投稿メッセージ一覧を取得(非同期処理)
+              // 投稿日時でソート
+              future: FirebaseFirestore.instance
+                .collection('posts')
+                .orderBy('date')
+                .get(),
+              builder: (context, snapshot) {
+                // データが取得できた場合
+                if(snapshot.hasData) {
+                  final List<DocumentSnapshot> documents = snapshot.data!.docs;
+                  //　取得して投稿メッセージ一覧を元にリスト表示
+                  return ListView(
+                    children: documents.map((document) {
+                      return Card(
+                        child: ListTile(
+                          title: Text(document['text']),
+                          subtitle: Text(document['email']),
+                        ),
+                      );
+                    }).toList(),
+                  );
+                }
+                // データが読込中の場合
+                return const Center(
+                  child: Text('読込中...'),
+                );
+              },
+            ),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
